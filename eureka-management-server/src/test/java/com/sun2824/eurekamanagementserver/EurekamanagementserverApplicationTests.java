c42a616d390101875d350e0e0f9f5ca8f346a866
leeyounghoon2824 <yh.lee@infinigru.com>
2022-01-03 17:30:16 +0900

    gateway및eureka관리서버 추가

---
+++ eureka-management-server/src/test/java/com/sun2824/eurekamanagementserver/EurekamanagementserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun2824.eurekamanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekamanagementserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gatewayserver/src/test/java/com/sun2824/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun2824.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
