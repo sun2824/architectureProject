5c83176b72c7327011b490e441472a9110899e43
leeyounghoon2824 <yh.lee@infinigru.com>
2022-01-04 15:28:27 +0900

    service add

---
+++ dashboardService/src/test/java/com/sun2824/dashboardservice/DashboardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun2824.dashboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- eureka-management-server/src/main/java/com/sun2824/eurekamanagementserver/EurekamanagementserverApplication.java
+++ eureka-management-server/src/main/java/com/sun2824/eurekamanagementserver/EurekamanagementserverApplication.java
@@ -2,8 +2,10 @@ package com.sun2824.eurekamanagementserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekamanagementserverApplication {
 
     public static void main(String[] args) {
