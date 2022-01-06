c0e6cbd946fc8d033f0106102bdd9d24eff7d917
leeyounghoon2824 <leeyounghoon2824>
2022-01-06 22:19:13 +0900

    dataservice 추가

---
+++ dataService/src/test/java/com/sun2824/dataservice/DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun2824.dataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
