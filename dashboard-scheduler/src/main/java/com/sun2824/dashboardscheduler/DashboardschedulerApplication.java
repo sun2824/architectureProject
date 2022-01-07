52d21f368ab3cd2db321d5d3464810c2cc2761fe
leeyounghoon2824 <yh.lee@infinigru.com>
2022-01-07 13:09:02 +0900

    dashboard-scheduler update

---
+++ dashboard-scheduler/src/main/java/com/sun2824/dashboardscheduler/DashboardschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.sun2824.dashboardscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardschedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardschedulerApplication.class, args);
+    }
+
+}
---
+++ dashboard-scheduler/src/test/java/com/sun2824/dashboardscheduler/DashboardschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun2824.dashboardscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardschedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
