56b373bc853590bb744c75cd6425f73991b08b48
leeyounghoon2824 <yh.lee@infinigru.com>
2022-01-18 17:54:15 +0900

    KafkaListener add

---
+++ dashboard-service/src/main/java/com/sun2824/dashboardservice/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.sun2824.dashboardservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@EnableWebSocket
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
--- dashboard-service/src/main/java/com/sun2824/dashboardservice/kafka/KafkaListener.java
+++ dashboard-service/src/main/java/com/sun2824/dashboardservice/kafka/KafkaListener.java
@@ -1,9 +1,17 @@
 package com.sun2824.dashboardservice.kafka;
 
 
-@Service
-public class KafkaListener {
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-    @KafkaListener(topics = "dashboardData" , groupId = "test")
-    public Strin
+@Service
+@Component
+public class KafkaListener {
+    SimpMessageSendingOperations messagingTemplate;
+
+    @org.springframework.kafka.annotation.KafkaListener(topics = "dashboardData" , groupId = "test")
+    public void dashboardDataMessage(String message){
+        messagingTemplate.convertAndSend("/topic/dashboardData", message);
+    }
 }
