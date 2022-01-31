c39c4894d93a84e0d13d074c7fcbcf70c66aefc2
leeyounghoon2824 <leeyounghoon2824>
2022-01-31 22:58:01 +0900

    개발 시작

--- eureka-management-server/src/main/java/com/sun2824/eurekamanagementserver/EurekamanagementserverApplication.java
+++ eureka-management-server/src/main/java/com/sun2824/eurekamanagementserver/EurekamanagementserverApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekamanagementserverApplication {
 
+    //유레카 클라이언트 관리하는 서버 어플리케이션
+    
     public static void main(String[] args) {
         SpringApplication.run(EurekamanagementserverApplication.class, args);
     }
